5835964e9b3b3d845aa56f12624477a6d10a43d3
luisillo619 <luiscarlosrangel619@gmail.com>
2023-02-02 22:02:44 -0600

    Deploy actualizado

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import store from "./redux/store/index";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-
+axios.defaults.baseURL = "https://pf-back-production-f70b.up.railway.app/";
 root.render(
   <React.StrictMode>
     <Provider store={store}>
