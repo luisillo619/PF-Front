29025f53e8c97a980988ed94da882c5486cb396a
MrAleew <andrespastorf@gmail.com>
2023-02-06 16:26:29 -0500

    FavoritesLs

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -7,12 +7,11 @@ import {
   ADD_PRODUCT_CART,
   FILTER_BY_LANDING_PAGE,
   ORDER_DETIALS,
-
+  GET_ALL_USERS,
   GET_ONE_USER,
   GET_COMMENTS_PRODUCT,
   POST_NEW_COMMENT,
   ERRORS,
-
 } from "../types/index";
 
 import Cookies from "js-cookie";
@@ -115,10 +114,10 @@ export const login = (form, setLoading, setResponse) => async () => {
 };
 
 export const register =
-  (form, setLoading, setResponse, setErrorRegister) => async () => {
+  (form, setLoading, setResponse, setErrorRegister,  setLoadingButton) => async () => {
     try {
       await axios.post(`http://localhost:3001/register`, { form });
-      setLoading(false);
+      setLoading(true);
       setResponse(true);
       setErrorRegister(null);
       setTimeout(() => {
