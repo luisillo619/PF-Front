1c689dadaaf234bbb51e3973bf7ed278cbb0781a
MrAleew <andrespastorf@gmail.com>
2023-02-06 23:02:50 -0500

    FavoritesOk

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -1,23 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Product from "../Products/Product";
 
+export default function Favorites() {
+  const [favorites, setFavorites] = useState([]);
 
-const Favorites = () => {
- 
-  //const favorites = JSON.parse(localStorage.getItem("favorites"));
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
 
-  //const handleClearFavs = () => {
-    //localStorage.removeItem("favorites");
-    //setUpdate(!update);
-  //};
-  let favorites = JSON.parse(localStorage.getItem("favorites"))
-
-  if (favorites) {
-    favorites.forEach(product => {
-      console.log(product.name)
-    });
-  } else {
-    console.log("No tienes productos marcados como favoritos")
-  }
- 
-};
-
-export default Favorites;
+  return (
+    <div className="favorites">
+      <h1>Mis productos favoritos</h1>
+      <div className="favorites-list">
+        {favorites.map(product => (
+          <Product
+            key={product.id}
+            id={product.id}
+            productName={product.productName}
+            productImage={product.productImage}
+            productDescription={product.productDescription}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
